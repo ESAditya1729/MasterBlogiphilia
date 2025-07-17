60df45a1adbbebb1e99b140b69fa835d677e9522
ESAditya1729 <adityamukherjee09949@gmail.com>
2025-07-17 17:07:55 +0530

    Fixed a backend issue

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // optional if you're serving static files
 
@@ -61,7 +64,7 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => {
   console.log('✅ MongoDB connected');
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 1000;
   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 })
 .catch((err) => {
