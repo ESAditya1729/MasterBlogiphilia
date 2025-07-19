c1afa92d05d3dfd10382d1fc2280c4ede996f072
ESAditya1729 <adityamukherjee09949@gmail.com>
2025-07-20 00:37:53 +0530

    fixed profile pictures for user lists

--- client/src/components/UI/SearchBar.jsx
+++ client/src/components/UI/SearchBar.jsx
@@ -139,7 +139,7 @@ const SearchBar = () => {
                         <img
                           src={
                             user.profilePicture
-                              ? `http://localhost:5000/uploads/${user.profilePicture}`
+                              ? `${process.env.REACT_APP_API_BASE_URL}${user.profilePicture}`
                               : "https://via.placeholder.com/40"
                           }
                           alt={user.username}
