90b7c210c038baf4204b32f365c8fa3f458f3654
ESAditya1729 <adityamukherjee09949@gmail.com>
2025-07-27 03:05:17 +0530

    Added Stats Routes in the backend-Fixed middleware export

--- server/routes/statsRoutes.js
+++ server/routes/statsRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUserStats } from "../controllers/statsController.js";
-import protect from "../middleware/auth.js";
+import { protect }  from "../middleware/auth.js";
 
 const router = express.Router();
 
